84963d31d4d896dd52563840700111040595ad39
Paweł Strużyński <easypawelek@gmail.com>
2024-02-20 15:56:38 +0100

    Zajęcia 1

---
+++ main.py
@@ -0,0 +1,38 @@
+# importowanie bibliotek
+import math
+import random
+
+# deklaracja zmiennych
+a = 2
+b = 3
+c = 4
+t = [5, 6, 7]
+
+# wypisanie
+print("Zmienna a = ", a)
+print("Zmienna b = ", b)
+print("Zmienna c = ", c)
+print("Tablica t = ", t[0], t[1], t[2])
+
+# ZADANIE 1
+# Wypisać 5 funkcji predefiniowanych
+
+# pierwsza funkcja predefiniowana (biblioteka math)
+print("Pierwiastek z c = ", math.sqrt(c))
+# druga funkcja predefiniowana (biblioteka random)
+print("Losowa liczba z tablicy t = ", random.choice(t))
+# trzecia funkcja predefiniowana (biblioteka math)
+print("Tangens 90 stopni = ", math.tan(90))
+# czwarta funkcja predefiniowana
+print("Wielkość listy t =", len(t))
+# piąta funkcja predefiniowana
+print("Spis właściwości i metod:", dir())
+
+# ZADANIE 2
+# Funkcja nazwa + działanie + wywołać ją
+
+# ZADANIE 3
+# Funkcje ze zmiennymi funkcyjnymi i globalnymi
+
+# ZADANIE 4
+# Przekazanie funkcji jako argument
